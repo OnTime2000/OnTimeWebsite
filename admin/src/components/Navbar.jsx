b4e55b1047865578400df36880417d37d4ca1249
OnTime2000 <OnTimeCustomGifts@gmail.com>
2025-08-02 23:54:17 +0530

    Seventeenth Commit

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 
-const Navbar = ({setToken}) => {
+const Navbar = ({ setToken }) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    <div className='relative flex justify-center py-4'>
+      <img className='w-48' src={assets.logo} alt="Logo" />
+      <button 
+        onClick={() => setToken('')} 
+        className='absolute right-4 top-1/2 -translate-y-1/2 bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'
+      >
+        Logout
+      </button>
     </div>
   )
 }
